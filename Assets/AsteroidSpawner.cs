c7fa069cbb74f352671d79c8d1c77730bf59812b
James Ma <james168ma@gmail.com>
2020-02-23 11:46:51 -0800

    james first commit

---
+++ Assets/AsteroidSpawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidSpawner : MonoBehaviour
+{
+    [SerializeField] GameObject someName;
+    public void CreateNewAsteroid(float x, float y)
+    {
+        Instantiate(someName, new Vector3(x, y, 0), Quaternion.identity);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreateNewAsteroid(0,0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
