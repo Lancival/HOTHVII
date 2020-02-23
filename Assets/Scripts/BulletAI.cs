8de8ef81df99f42475ff08dff1c10bb646af9b30
Chrizon123 <christianloanzon322@gmail.com>
2020-02-23 13:09:23 -0800

    added prefabs, destroy on invis

---
+++ Assets/Scripts/BulletAI.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletAI : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D playerBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerBody = GetComponent<Rigidbody2D>();
+        float currentAngle = playerBody.rotation * Mathf.Deg2Rad;
+        playerBody.velocity = new Vector2(-Mathf.Sin(currentAngle) * speed, Mathf.Cos(currentAngle) * speed);
+    }
+}
