ba7be713290a3e7fcaee4d65826efd33e881f7f2
Chrizon123 <christianloanzon322@gmail.com>
2020-02-23 12:15:57 -0800

    updated character controller
    now has screenwrapping

--- Assets/Scripts/CharacterMoveController.cs
+++ Assets/Scripts/CharacterMoveController.cs
@@ -6,6 +6,7 @@ public class CharacterMoveController : MonoBehaviour
 {
     public float moveForce; // force based movement, adds to the 'space' feel
     public float rotationSpeed; // should rotation be force-based? causes player to think about compensation
+    public float frictionForce;
     private Rigidbody2D playerBody;
 
     private float horizInput;
@@ -30,9 +31,35 @@ public class CharacterMoveController : MonoBehaviour
     void FixedUpdate()
     {
         float currentAngle = playerBody.rotation * Mathf.Deg2Rad;
+        Vector2 friction = playerBody.velocity * -frictionForce;
         direction = new Vector2(-Mathf.Sin(currentAngle), Mathf.Cos(currentAngle));
         if(vertiInput == 1)
             playerBody.AddForce(direction * moveForce);
         playerBody.rotation += rotationSpeed * horizInput * -1;
+        playerBody.velocity += friction;
+        Debug.Log(playerBody.position);
+
+        float playerX = playerBody.position.x;
+        float playerY = playerBody.position.y;
+        bool willWrap = false;
+        if (playerX > 10.5 || playerX < -10.5)
+        {
+            playerX = playerX * -1;
+            if (playerX > 0) playerX += -0.5f;
+            else playerX += 0.5f;
+            willWrap = true;
+        }
+        if (playerY > 5.5 || playerY < -5.5)
+        {
+            playerY = playerY * -1;
+            if (playerY > 0) playerY += -0.5f;
+            else playerY += 0.5f;
+            willWrap = true;
+        }
+
+        if (willWrap)
+        {
+            playerBody.MovePosition(new Vector2(playerX, playerY));
+        }
     }
 }
