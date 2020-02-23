7bb02f3dd03a9f8c9d57269601038c5f16377953
James Ma <james168ma@gmail.com>
2020-02-23 13:12:48 -0800

    added more asteroid functionality

--- Assets/Scripts/AsteroidSpawner.cs
+++ Assets/Scripts/AsteroidSpawner.cs
@@ -12,9 +12,10 @@ public class AsteroidSpawner : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        for(int i = 0; i < 10; i++) {
-          CreateNewAsteroid(Random.Range(-10.0f, 10.0f),Random.Range(-10.0f, 10.0f));
-        }
+        for(int i = 0; i < 5; i++) CreateNewAsteroid(Random.Range(-10.0f, -9.0f), Random.Range(-7.0f, 7.0f));   // Left
+        for(int i = 0; i < 5; i++) CreateNewAsteroid(Random.Range(-10.0f, 10.0f), Random.Range(-8.0f, -7.0f));  // Bottom
+        for(int i = 0; i < 5; i++) CreateNewAsteroid(Random.Range(9.0f, 10.0f), Random.Range(-7.0f, 7.0f));     // Right
+        for(int i = 0; i < 5; i++) CreateNewAsteroid(Random.Range(-10.0f, 10.0f), Random.Range(7.0f, 8.0f));    // Top
     }
 
     // Update is called once per frame
