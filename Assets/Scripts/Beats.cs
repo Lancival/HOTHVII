eaa3e06094fe46afa3825dfa620a507cfe832f9f
Richard Cheng <rhocheng@g.ucla.edu>
2020-02-23 14:32:17 -0800

    Beat indicator sound

--- Assets/Scripts/Beats.cs
+++ Assets/Scripts/Beats.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-// Usage: Attach to an object in any scene where you need beats! Manipulate the beats
+// Usage: Attach to the Main Camera, which should have an audio source on it. Manipulate the beats
 // by changing values in Globals.
 
 public class Beats : MonoBehaviour {
@@ -10,9 +10,11 @@ public class Beats : MonoBehaviour {
     private List<int> beat_list = new List<int>(new int[] {1});
     private int list_sum = 1;
     private int beats = 0; // Number of beats started, only off_beat when equal to 0
+    private AudioSource indicator;
 
     // Start is called before the first frame update
     public void Start() {
+        indicator = gameObject.GetComponent<AudioSource>();
         StartCoroutine(Beat());
     }
 
@@ -27,7 +29,9 @@ public class Beats : MonoBehaviour {
     }
 
     public IEnumerator OffBeat() {
-        yield return new WaitForSeconds(Globals.BEAT_LEN);
+        yield return new WaitForSeconds(Globals.BEAT_LEN / 2);
+        indicator.Play(0);
+        yield return new WaitForSeconds(Globals.BEAT_LEN / 2);
         beats--;
         if (beats == 0)
             Globals.ON_BEAT = false;
