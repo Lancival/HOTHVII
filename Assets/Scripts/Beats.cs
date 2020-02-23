679d1d60644e961f28ed741252c10b8226a9c1d2
Richard Cheng <rhocheng@g.ucla.edu>
2020-02-23 11:03:39 -0800

    Beats finished

---
+++ Assets/Scripts/Beats.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Beats : MonoBehaviour {
+	private GameObject BEAT_TEST;
+
+    // Start is called before the first frame update
+    public void Start() {
+    	BEAT_TEST = GameObject.Find("Beat Test");
+        StartCoroutine(Beat());
+    }
+
+    public IEnumerator Beat() {
+    	while (true) {
+    		yield return new WaitForSeconds(Globals.BEAT_TIME);
+    		Globals.ON_BEAT = true;
+    		BEAT_TEST.SetActive(true);
+    		Debug.Log("Beat!");
+    		yield return new WaitForSeconds(Globals.BEAT_PM);
+    		Globals.ON_BEAT = false;
+    		BEAT_TEST.SetActive(false);
+    		Debug.Log("No Beat!");
+    	}
+    }
+}
---
+++ Assets/Scripts/Globals.cs
@@ -0,0 +1,5 @@
+﻿public static class Globals {
+    public static bool ON_BEAT = false;
+    public static float BEAT_TIME = 1.0f; // Time between beats
+    public static float BEAT_PM = 0.1f; // Leniency after beat
+}
