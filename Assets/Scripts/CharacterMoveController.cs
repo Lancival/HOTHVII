eb3ae759b346d338e4f3a3350c9e1238020ad8b3
Chrizon123 <christianloanzon322@gmail.com>
2020-02-23 11:34:36 -0800

    character controller
    added basics of character controller
    up arrow key to move 'forward' relative to angle
    left/right arrow keys to rotate the character

---
+++ Assets/Scripts/CharacterMoveController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMoveController : MonoBehaviour
+{
+    public float moveForce; // force based movement, adds to the 'space' feel
+    public float rotationSpeed; // should rotation be force-based? causes player to think about compensation
+    private Rigidbody2D playerBody;
+
+    private float horizInput;
+    private float vertiInput;
+    private Vector2 direction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerBody = GetComponent<Rigidbody2D>();
+        direction = new Vector2(0, 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizInput = Input.GetAxis("Horizontal");
+        vertiInput = Input.GetAxis("Vertical");
+        //direction = new Vector2(horizInput, vertiInput);
+    }
+
+    void FixedUpdate()
+    {
+        float currentAngle = playerBody.rotation * Mathf.Deg2Rad;
+        direction = new Vector2(-Mathf.Sin(currentAngle), Mathf.Cos(currentAngle));
+        if(vertiInput == 1)
+            playerBody.AddForce(direction * moveForce);
+        playerBody.rotation += rotationSpeed * horizInput * -1;
+    }
+}
