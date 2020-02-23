5bd356f15c422aab3054975e37ad75df7e6be073
Richard Cheng <rhocheng@g.ucla.edu>
2020-02-23 12:40:38 -0800

    Worked on gameplay UI

--- Assets/Scripts/Globals.cs
+++ Assets/Scripts/Globals.cs
@@ -2,4 +2,5 @@
     public static bool ON_BEAT = false;
     public static float BEAT_TIME = 3.0f; // Time between beats
     public static float BEAT_LEN = 0.1f; // Leniency after beat
+    public static int SCORE = 0;
 }
---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Score : MonoBehaviour {
+    // Start is called before the first frame update
+    void Start() {
+        Globals.SCORE = 0; // Reset score when starting new game
+    }
+
+    // Update is called once per frame
+    void Update() {
+        gameObject.GetComponent<TextMeshProUGUI>().SetText("Score: " + Globals.SCORE.ToString());
+    }
+}
