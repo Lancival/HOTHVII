4d3b32ce9fc177cb9a2057e779738fba7a7dac16
James Ma <james168ma@gmail.com>
2020-02-23 16:02:38 -0800

    implementing lives

--- Assets/Scripts/Globals.cs
+++ Assets/Scripts/Globals.cs
@@ -4,6 +4,7 @@
     public static float BEAT_LEN = 0.25f; // Leniency after beat
     public static int SCORE = 0;
     public static int COMBOS = 0;
+    public static int LIVES = 3;
     public static float VIEW_Y_RADIUS = 5;
     public static float VIEW_X_RADIUS = 8;
 }
