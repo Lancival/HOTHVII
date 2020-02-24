2b20539ca3038ca9f0d19ecd145733f7fdae80bf
Chrizon123 <christianloanzon322@gmail.com>
2020-02-23 17:35:03 -0800

    shoot logic

--- Assets/Scripts/Shoot.cs
+++ Assets/Scripts/Shoot.cs
@@ -6,7 +6,7 @@ public class Shoot : MonoBehaviour
 {
     public GameObject bullet;
 
-    private bulletType;
+    private int bulletType;
     private Rigidbody2D playerBody;
     // Start is called before the first frame update
     void Start()
