2ce38eb55f38d24aa47ed5175faed74960eae6fe
Richard Cheng <rhocheng@g.ucla.edu>
2020-02-23 16:28:53 -0800

    Settings completed

--- Assets/Scripts/Globals.cs
+++ Assets/Scripts/Globals.cs
@@ -6,4 +6,6 @@
     public static int COMBOS = 0;
     public static float VIEW_Y_RADIUS = 5;
     public static float VIEW_X_RADIUS = 8;
+    public static float VOLUME = 100.0f; // Audio volume
+    public static int DIFFICULTY = 0; // 0 = easy, 1 = normal, 2 = hard
 }
---
+++ Assets/Scripts/Settings.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class Settings : MonoBehaviour {
+    // Start is called before the first frame update
+
+	private GameObject slider;
+	private GameObject buttons;
+
+    void Start() {
+    	slider = GameObject.Find("Audio Volume Slider");
+    	buttons = GameObject.Find("Buttons");
+        slider.GetComponent<Slider>().value = Globals.VOLUME;
+        slider.GetComponent<Slider>().onValueChanged.AddListener(delegate {setAudioVolume();});
+        buttons.transform.GetChild(Globals.DIFFICULTY).gameObject.GetComponent<Button>().interactable = false;
+        for (int i = 0; i < 3; i++)
+        	buttons.transform.GetChild(i).gameObject.GetComponent<Button>().onClick.AddListener(setDifficulty);
+    }
+
+    private void setDifficulty() {
+    	buttons.transform.GetChild(Globals.DIFFICULTY).gameObject.GetComponent<Button>().interactable = true;
+    	Globals.DIFFICULTY = EventSystem.current.currentSelectedGameObject.transform.GetSiblingIndex();
+    	buttons.transform.GetChild(Globals.DIFFICULTY).gameObject.GetComponent<Button>().interactable = false;
+    }
+
+    private void setAudioVolume() {
+    	Globals.VOLUME = EventSystem.current.currentSelectedGameObject.GetComponent<Slider>().value;
+    }
+}
