6e9224b569f264d975263f5a1233f6af1382b0b4
Richard Cheng <rhocheng@g.ucla.edu>
2020-02-23 16:46:25 -0800

    Cleanup

--- Assets/Scripts/GibOnCollide.cs
+++ Assets/Scripts/GibOnCollide.cs
@@ -2,21 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GibOnCollide : MonoBehaviour
-{
-    public int value;
+public class GibOnCollide : MonoBehaviour {
+    [SerializeField] private int baseScore;
 
-    void OnTriggerEnter2D(Collider2D collider)
-    {
+    void OnTriggerEnter2D(Collider2D collider) {
 
         //Instantiate(sound, transform.position, transform.rotation);
-        //ManageGame.score+= value;
         if(gameObject.name == "Player") { // if colliding object is a player
             Globals.LIVES--;              // subtract a life
             Debug.Log("lose life");
             Debug.Log(Globals.LIVES + " lives left");
             if(Globals.LIVES == 0) { Destroy(gameObject); } // Destroy player once 0 lives
         } else {
+            Globals.SCORE += baseScore + Globals.COMBOS * Globals.COMBOS;
             Destroy(gameObject);
         }
     }
