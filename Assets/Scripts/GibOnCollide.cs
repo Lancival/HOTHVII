aa14ba019644d84924003e029c2097b82add5170
Richard Cheng <rhocheng@g.ucla.edu>
2020-02-23 19:46:58 -0800

    Fixed collision

--- Assets/Scripts/GibOnCollide.cs
+++ Assets/Scripts/GibOnCollide.cs
@@ -8,15 +8,14 @@ public class GibOnCollide : MonoBehaviour {
     void OnTriggerEnter2D(Collider2D collider) {
         
         //Instantiate(sound, transform.position, transform.rotation);
-        if (gameObject.name == "Player" && collider.gameObject.layer != 11) { // if colliding object is a player
+        if (gameObject.name == "Player" && collider.gameObject.layer == 8) { // if colliding object is a player
             Globals.LIVES--;              // subtract a life
             Debug.Log("lose life");
             Debug.Log(Globals.LIVES + " lives left");
-            if(Globals.LIVES == 0) { Destroy(gameObject); } // Destroy player once 0 lives
-        } else if (collider.gameObject.layer == 8 && gameObject.layer == 8) {
-            // If two asteroids collide, do nothing
+            if (Globals.LIVES == 0) { Destroy(gameObject); } // Destroy player once 0 lives
+            Destroy(collider.gameObject);
+        } else if (gameObject.layer == 11)
             return;
-        }
         else if (gameObject.layer == 9 && GameObject.Find("Player").GetComponent<Shoot>().PowerUpVal(1))
         {
             
@@ -25,6 +24,7 @@ public class GibOnCollide : MonoBehaviour {
             Debug.Log(gameObject.layer);
             Globals.SCORE += baseScore + Globals.COMBOS * Globals.COMBOS;
             collider.gameObject.GetComponent<AudioSource>().Play(0);
+            Destroy(collider.gameObject);
             Destroy(gameObject);
         }
     }
