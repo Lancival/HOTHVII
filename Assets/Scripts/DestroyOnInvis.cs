814ae7560569bfdf379b6f9f3391eda49df2f96e
Chrizon123 <christianloanzon322@gmail.com>
2020-02-23 19:47:38 -0800

    Update DestroyOnInvis.cs

--- Assets/Scripts/DestroyOnInvis.cs
+++ Assets/Scripts/DestroyOnInvis.cs
@@ -4,7 +4,36 @@ using UnityEngine;
 
 public class DestroyOnInvis : MonoBehaviour
 {
+    bool willWrap = false;
+    private float BORDER = 0.1f;
     void OnBecameInvisible() {
-        Destroy(gameObject);
+        if (!willWrap)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            float playerX = transform.position.x;
+            float playerY = transform.position.y;
+            if (playerX > 8 + BORDER || playerX < -8 - BORDER)
+            {
+                playerX = playerX * -1;
+                if (playerX > 0) playerX -= BORDER;
+                else playerX += BORDER;
+            }
+            if (playerY > 5 + BORDER || playerY < -5 - BORDER)
+            {
+                playerY = playerY * -1;
+                if (playerY > 0) playerY -= BORDER;
+                else playerY += BORDER;
+            }
+            transform.position = (new Vector2(playerX, playerY));
+        }
+    }
+
+    void update()
+    {
+        GameObject.Find("Player").GetComponent<Shoot>().PowerUpSet(2);
+        willWrap = GameObject.Find("Player").GetComponent<Shoot>().PowerUpVal(2);
     }
 }
