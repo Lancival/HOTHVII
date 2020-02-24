83a3b1482a9b9dda3fa696b0539532ab70ada972
Chrizon123 <christianloanzon322@gmail.com>
2020-02-23 17:07:18 -0800

    powerups

---
+++ Assets/Scripts/PowerUp.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUp : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        PowerUpPlayer();
+        Destroy(gameObject);
+    }
+
+    void PowerUpPlayer()
+    {
+
+    }
+}
---
+++ Assets/Scripts/TridentPower.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TridentPower : PowerUp
+{
+    public new void PowerUpPlayer()
+    {
+        Debug.Log("trident");
+    }
+}
